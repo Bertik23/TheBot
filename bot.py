be9a3e814b6988dc608410c9726bff0ef7934f9c
Bertik23 <bertikxxiii@gmail.com>
2020-05-28 11:10:22 +0200

    adaam

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
-from random import choice
+import random
+import numpy as np
+from random import choice, randint
 import os
 import math
 import requests
@@ -8,6 +10,7 @@ from botFunctions import getZmena, embed, command, gymso
 import praw
 import prawcore
 import ksoftapi
+import re
 
 kclient = ksoftapi.Client(os.environ.get("ksoft_token", None))
 
@@ -57,12 +60,19 @@ async def on_message(message):
 	if a >= spamValue and message.author.name != "TheBot":
 		await message.channel.send(f"{message.author.mention} nespamuj!")
 
-	for i in ["dobrý den","brý den","čau","ahoj", "zdravíčko", "tě péro","zdárek párek","tě guli", "čus"]:
-		if i in message.content.lower() and not message.author.bot:
+	for i in ["hi","dobrý den","brý den","čau","ahoj", "zdravíčko", "tě péro", "těpéro", "zdárek párek","tě guli", "čus"]:
+		if re.search(f"(\W|^){i}(\W|$)", message.content, re.I) and not message.author.bot:
 			await message.channel.send(f"Hello {message.author.mention}")
+			break
 
 	if "kdy" in message.content.lower() and "aktualizace" in message.content.lower():
 		await message.channel.send("Kdo ví")
+
+	if (re.search("(\W|^)a+da+m(\W|$)", message.content, re.I)) and not message.author.bot:
+		await message.channel.send(f"A{randint(0,20)*'a'}d{randint(1,20)*'a'}m {choice(['je gay','neumí olí','už nevytírá anály',''])}")
+
+	if (re.search("thebot", message.content, re.I) or client.user.mentioned_in(message)) and not message.author.bot:
+		await message.channel.send(choice(["Slyšel jsem snad moje jméno?",f"{message.author.mention} ty ses opovážil vyslovit moje jméno?","Ještě jednou tu zazní moje jméno a uvidíte.",f"Chceš do držky {message.author.mention}?",f"Tak to je naposledy co jste {message.author.mention} vyděli."]))
 
 	if message.tts and not message.author.bot:
 		await message.channel.send(f"Hej ty {message.author.mention}, žádný ttska tady.", tts = True)
