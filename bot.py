26f80f947fe0c62e22dfcb0e877760ffce60e0cb
Bertik23 <bertikxxiii@gmail.com>
2020-06-08 19:07:13 +0200

    jiří

--- bot.py
+++ bot.py
@@ -83,6 +83,9 @@ async def on_message(message):
 	if (re.search("(\W|^)a+da+m(\W|$)", message.content, re.I)) and not message.author.bot:
 		await message.channel.send(f"A{randint(0,20)*'a'}d{randint(1,20)*'a'}m {choice(['je gay','neumí olí','už nevytírá anály',''])}")
 
+	if (re.search("(\W|^)ji+ří+(\W|$)", message.content, re.I)) and not message.author.bot:
+		await message.channel.send(f"Jiří {choice([' je buzík',' nic neumí'])}")
+
 	if (re.search("thebot", message.content, re.I) or client.user.mentioned_in(message)) and not message.author.bot:
 		await message.channel.send(choice(["Slyšel jsem snad moje jméno?",f"{message.author.mention} ty ses opovážil vyslovit moje jméno?","Ještě jednou tu zazní moje jméno a uvidíte.",f"Chceš do držky {message.author.mention}?",f"Tak to je naposledy co jste {message.author.mention} viděli."]))
 
