20a3327ba47605e289da65e356e47c8ea035c187
Bertik23 <39457484+Bertik23@users.noreply.github.com>
2020-09-20 15:56:42 +0200

    pls worknow

--- botFunctions.py
+++ botFunctions.py
@@ -14,6 +14,7 @@ import plotly.graph_objects as go
 import numpy as np
 import io
 import pprint
+import smaz
 
 commandPrefix: str = None
 wClient = wolframalpha.Client("TV7GVY-8YLJ26PPK9")
@@ -206,3 +207,52 @@ def getLastInstaPost(user):
 
     print("test")
     return instaJson["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]
+
+
+
+def encrypt(text_to_encrypt, encryption_base):
+    digits = []
+    for i in range(48,48 + encryption_base):
+        try:
+            digits.append(bytes(chr(i),"utf-8").decode("utf-8"))
+        except UnicodeEncodeError:
+            pass
+    text = smaz.compress(text_to_encrypt)
+    textInts = [i for i in text]
+    textNum = ""
+    result = -1
+    remainder = -1
+    cipher = """"""
+    for i in textInts:
+        m = str(i)
+        for _ in range(3-len(m)):
+            m = f"0{m}"
+        textNum = f"{textNum}{m}"
+    try:
+        result = int(textNum)
+    except:
+        result = 0
+    while result != 0:
+        remainder = result % len(digits)
+        result = result // len(digits)
+        cipher = f"{digits[remainder]}{cipher}"
+    return cipher
+
+
+def decrypt(text_to_decrypt, encryption_base):
+    digits = []
+    for i in range(48,48 + encryption_base):
+        digits.append(bytes(chr(i),"utf-8").decode("utf-8"))
+    cipher = text_to_decrypt
+    num = 0
+    power = len(cipher)-1
+    text = ""
+    for c in cipher:
+        num += (digits.index(c) * (len(digits) ** power))
+        power -= 1
+    for i in range(3-(len(str(num))%3) if len(str(num))%3 != 0 else 0):
+        num = f"0{num}"
+    num = str(num)
+    n = 3
+    nums = [int(num[i:i+n]) for i in range(0, len(num), n)]
+    return smaz.decompress(bytes(nums))
---
+++ pigarImports.py
@@ -0,0 +1,5 @@
+"""This file is for pigar for making requirements.txt
+
+It adds essential packages to the requirement.txt file, that are not imported anywhere"""
+
+import kaleido
