93b0a39ec76ca935f9ef8271a9f665541084c888
Bertik23 <39457484+Bertik23@users.noreply.github.com>
2020-10-12 12:23:32 +0200

    new hlášky for thebot

--- bot.py
+++ bot.py
@@ -128,7 +128,7 @@ async def on_message(message):
 		await message.delete()
 
 	if (re.search("thebot", message.content, re.I) or client.user.mentioned_in(message)) and not message.author.bot:
-		await message.channel.send(choice(["Slyšel jsem snad moje jméno?",f"{message.author.mention} ty ses opovážil vyslovit moje jméno?","Ještě jednou tu zazní moje jméno a uvidíte.",f"Chceš do držky {message.author.mention}?",f"Tak to je naposledy co jste {message.author.mention} viděli."]))
+		await message.channel.send(choice(["Slyšel jsem snad moje jméno?",f"{message.author.mention} ty ses opovážil vyslovit moje jméno?","Ještě jednou tu zazní moje jméno a uvidíte.",f"Chceš do držky {message.author.mention}?",f"Tak to je naposledy co jste {message.author.mention} viděli.", f"Naklepu ti řízek ty pomeranči {message.author.mention}", f"{message.author.mention} zmaluju ti ksicht tak, že tě ani Adam nepozná", f"Urazim ti tvé intimní partie, btw {klubik.get_member(690510410763337768)} smrdí", f"Jestli nepřestaneš psát moje jméno, tak ti pošlu fotku {klubik.get_member(476286797048184882)} PP"]))
 
 	if message.tts and not message.author.bot:
 		await message.channel.send(f"Hej ty {message.author.mention}, žádný ttska tady.", tts = True)
